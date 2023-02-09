32986317b7a184cc99e16b3b7ea9dbba92357ef4
6231368521 <blastfactorz@gmail.com>
2023-02-09 15:27:03 +0700

    Add MongoDB

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 dotenv.config({path:'./config/config.env'});
 
+connectDB();
+
 const app = express();
+
+//Body parser
+app.use(express.json());
 
 const hospitals = require('./routes/hospitals');
 app.use('/api/v1/hospitals', hospitals);
@@ -16,4 +22,10 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+
+    server.close(() => process.exit(1));
+})
